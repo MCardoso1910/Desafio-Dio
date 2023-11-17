9a7decd5d166a0de6690fbbdb41761e895c47455
Matheus Cardoso da costa <Theuscardosodacosta@gmail.com>
2023-11-17 18:10:48 -0300

    Desafio da dio

---
+++ index.js
@@ -0,0 +1,14 @@
+let nivelDeXP = "12.000"
+let nome = "Naruto"
+
+if(nivelDeXP <= 1.000 ){
+    console.log("Nivel de xp ferro: ")
+}else if(nivelDeXP === 1.001  <= 2.000){
+    console.log("Nivel de xp broze: ")
+}else(nivelDeXP === 2.001  <= 5.000)
+
+
+
+console.log("o herói de nome: " + nome)
+console.log("o nivel de xp é: " +  nivelDeXP)
+
